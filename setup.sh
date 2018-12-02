dc2a2a3e024f753fa32adce8725fa4a0522c2dc2
bakanovkirill <kirill.bakanov@gmail.com>
2018-12-02 09:59:43 +0200

    Fixed setup file

--- setup.sh
+++ setup.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt-get update
+sudo apt-get install python3.6
+export PIPENV_VENV_IN_PROJECT=1
+cat <<EOF > ./.env
+APP_NAME="meneto"
+APP_SETTINGS="project.server.config.DevelopmentConfig"
+FLASK_DEBUG=1
+EOF
 sudo pip install pipenv
 pipenv install --dev
 pipenv run ./db-setup.sh
